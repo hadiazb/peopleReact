48a44c57460fc4efedf71090dcc477765f84d075
hadiazb <hadiazb@unal.edu.co>
2020-07-12 23:00:58 -0500

    Creando layout del personaje

--- src/script/App.js
+++ src/script/App.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import { Footer } from './Components/Footer/index';
+import { Header } from './Components/Header/index';
+import { Formulary } from './Components/Formulario/index';
 
 export const App = () => {
 	return (
 		<Fragment>
 			<GlobalStyle />
+			<Header />
+			<Formulary />
 			<Footer />
 		</Fragment>
 	);
